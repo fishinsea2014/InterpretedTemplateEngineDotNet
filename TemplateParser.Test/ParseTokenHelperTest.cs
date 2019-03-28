eb459ccba0320d904c70d058a8ba5d60bb41c6c5
Jason Qu <fishinsea2014@gmail.com>
2019-03-28 13:05:03 +1300

    Framework of the project

---
+++ TemplateParser.Test/ParseTokenHelperTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemplateParser.Test
+{
+    [TestClass]
+    public class ParseTokenHelperTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string template = "Hello [Name] ,live in [Address]";
+            ParseTokenHelper parseTokenHelper = new ParseTokenHelper();
+            var res=(List<string>)parseTokenHelper.ParseTemplate(template);
+            List<string> expected = new List<string>()
+            {
+                "Hello , ",
+                "[Name]",
+                ",live in ",
+                "[Address]"
+            };
+            Assert.AreEqual(res.Count, 4);
+        }
+    }
+}
